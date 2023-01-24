a4b5a8d8b9bb46cc44aef716af384dff01ce63a9
Rasmus Jensen <rasmusdj96@gmail.com>
2023-01-24 14:42:55 +0100

    Finished form

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Greeting } from "./Components/Greeting";
 import { Counter } from "./Components/Counter";
+import { Form } from "./Components/Form";
 
 function App() {
 	return (
 		<div className="App">
 			<Greeting />
 			<Counter />
+			<Form />
 		</div>
 	);
 }
---
+++ react-hooks/src/Components/Form.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+export const Form = () => {
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [phoneNumber, setPhoneNumber] = useState("");
+	const [email, setEmail] = useState("");
+	const [comment, setComment] = useState("");
+
+	const [message, setMessage] = useState("");
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		setMessage(`Hej ${firstName} ${lastName}, dit telefonnummer er "${phoneNumber}"
+		 din email er "${email}" og du har valgt at skrive "${comment}" i kommentarfeltet`);
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type="text"
+				id="firstName"
+				name="firstName"
+				placeholder="Fornavn"
+				onChange={(e) => setFirstName(e.target.value)}
+			/>
+			<br></br>
+			<input
+				type="text"
+				id="lastName"
+				name="lastName"
+				placeholder="Efternavn"
+				onChange={(e) => {
+					setLastName(e.target.value);
+				}}
+			/>
+			<br></br>
+			<input
+				type="number"
+				id="phoneNumber"
+				name="phoneNumber"
+				placeholder="Telefonnummer"
+				onChange={(e) => {
+					setPhoneNumber(e.target.value);
+				}}
+			/>
+			<br></br>
+			<input
+				type="mail"
+				id="email"
+				name="email"
+				placeholder="Email"
+				onChange={(e) => {
+					setEmail(e.target.value);
+				}}
+			/>
+			<br></br>
+			<textarea
+				id="comment"
+				name="comment"
+				placeholder="Indtast kommentar"
+				onChange={(e) => {
+					setComment(e.target.value);
+				}}
+			/>
+			<br></br>
+			<button type="submit">Submit</button>
+			<button type="reset">Reset</button>
+			<br></br>
+			<p>{message}</p>
+		</form>
+	);
+};
